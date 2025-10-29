9dfdc38cc1e9c8a253dfe9607e57f166f27740ad
Umerjamshaid <umerjamshiad481@gmail.com>
2025-10-29 05:50:21 -0700

    maps

--- maps.dart
+++ maps.dart
@@ -1,3 +1,33 @@
 void main() {
   // todo: Maps! Key/Value pairs
+  var toppings = {
+    "John": "Chikken tikka",
+    "umer": "Cheese",
+    "usman": "Pepporni",
+  };
+  print(toppings);
+  print(toppings["John"]);
+
+  // Show Value
+  print(toppings.values);
+  //Show Keys
+  print(toppings.keys);
+  //Shows length
+  print(toppings.length);
+
+  //Add Somthings
+  toppings["tim"] = "Tikka";
+  print(toppings);
+
+  // Add many things
+  toppings.addAll({"Tina": "Susage", "Ali": "Cheezy", "hamdan": "Chocolate"});
+  print(toppings);
+
+  // Remove things
+  toppings.remove("umer");
+  print(toppings);
+
+  //  Clear everything
+  toppings.clear();
+  print(toppings);
 }
